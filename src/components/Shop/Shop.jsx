960bd202d2efc92abc6cacb1038f62a0d8eff79d
monir-munna <monirhosen63219@gmail.com>
2023-03-31 21:33:57 +0600

    added cart component and calculation

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect( () => {
     fetch('products.json')
     .then(res => res.json())
     .then(data => setProducts(data))
   }, [])
+
+  const handleAddToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+  }
 
   return (
     <div className='shop-container'>
@@ -18,11 +25,12 @@ const Shop = () => {
           products.map(product => <Product 
             key={product.id}
             product = {product}
+            handleAddToCart={handleAddToCart}
             ></Product>)
         }
       </div>
       <div className="card-container">
-        <h4>Order Summary</h4>
+        <Cart cart={cart}></Cart>
       </div>
     </div>
   );
